57cc6f62e5c683e4bd54138908e8d0c7137469d7
Rado <sitcom93@abv.bg>
2025-05-26 23:14:01 +0300

    Initial commit

---
+++ CinemaApp.Data.Common/EntityConstants.cs
@@ -0,0 +1,33 @@
+﻿namespace CinemaApp.Data.Common
+{
+    public static class EntityConstants
+    {
+        public static class Movie
+        {
+            /// <summary>
+            /// Movie Title should be able to store text with length up to 150
+            /// </summary>
+            public const int TitleMaxLength = 150;
+
+            /// <summary>
+            /// Movie Genre should be able to store text with length up to 30
+            /// </summary>
+            public const int GenreMaxLength = 30;
+
+            /// <summary>
+            /// Movie Director should be able to store text with length up to 150
+            /// </summary>
+            public const int DirectorMaxLength = 150;
+
+            /// <summary>
+            /// Movie Description should be able to store text with length up to 1024
+            /// </summary>
+            public const int DescriptionMaxLength = 1024;
+
+            /// <summary>
+            /// Movie ImageUrl should be able to store text with length up to 2048 (refer URI RFC)
+            /// </summary>
+            public const int ImageUrlMaxLength = 2048;
+        }
+    }
+}
---
+++ CinemaApp/Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+﻿namespace CinemaApp.Web.Controllers
+{
+    using CinemaApp.Data;
+    using CinemaApp.Web.ViewModels.Movie;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    public class MovieController : Controller
+    {
+        private readonly CinemaAppDbContext dbContext;
+
+        public MovieController(CinemaAppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<AllMoviesIndexViewModel> movies = await this.dbContext
+                .Movies
+                .Select(m => new AllMoviesIndexViewModel
+                {
+                    Id = m.Id.ToString(),
+                    Title = m.Title,
+                    Genre = m.Genre,
+                    Director = m.Director,
+                    ReleaseDate = m.ReleaseDate.ToString("yyyy-MM-dd"),
+                    ImageUrl = m.ImageUrl
+                })
+                .ToListAsync();
+
+            return View(movies);
+        }
+    }
+}
